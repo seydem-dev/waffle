f8160352cdab1a32d6656b7f2ebda7f73456c48a
Seyon Demiroglu <seyon.demiroglu01@gmail.com>
2022-07-19 08:23:27 +0200

    Reduce code

--- contracts/PriceConverter.sol
+++ contracts/PriceConverter.sol
@@ -6,7 +6,7 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract PriceConverter {
 
-    AggregatorV3Interface internal priceFeed;
+    AggregatorV3Interface internal immutable priceFeed;
 
     /**
      * Oracle: Chainlink
@@ -19,12 +19,6 @@ contract PriceConverter {
     }
 
     function getLatestPrice() external view returns (int256 price) {
-        (
-            /*uint80 roundID*/,
-            price,
-            /*uint256 startedAt*/,
-            /*uint256 timeStamp*/,
-            /*uint80 answeredInRound*/
-        ) = priceFeed.latestRoundData();
+        (/*uint80 roundID*/, price, /*uint256 startedAt*/, /*uint256 timeStamp*/, /*uint80 answeredInRound*/) = priceFeed.latestRoundData();
     }
 }
